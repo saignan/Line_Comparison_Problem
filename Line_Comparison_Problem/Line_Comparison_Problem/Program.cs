47c36355c1b13f85dc4f123155c44cff3c74cec4
saignan <saignankarrolla695@gmail.com>
2023-04-19 21:13:15 +0530

    UseCase3.cs and UseCase4.cs are added and Program.cs is Updated

--- Line_Comparison_Problem/Line_Comparison_Problem/Program.cs
+++ Line_Comparison_Problem/Line_Comparison_Problem/Program.cs
@@ -8,7 +8,13 @@
             //UseCase1.UC1();
 
             //UseCase2
-            UseCase2.UC2();
+            //UseCase2.UC2();
+
+            //UseCase3
+            //UseCase3.UC3();
+
+            //Usecase4
+            UC4.UC();
         }
     }
 }
